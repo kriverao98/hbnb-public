e5f9a6f41345cacd987397f80fb2dd05e62090e8
kriverao98 <riveraocasiokoral29@gmail.com>
2024-07-31 01:58:03 -0400

    moved image files to specific folder, implemented classes on index and place file. working on script.js file, added function to fetch api for place details

--- 03-frontend/base_files/scripts.js
+++ 03-frontend/base_files/scripts.js
@@ -3,6 +3,22 @@
   Please, follow the project instructions to complete the tasks.
 */
 
+async function fetchPlace_details() {
+  const placeDetailsApi = "/places";
+
+  try {
+    const fetchRequest = await fetch (placeDetailsApi);
+
+    if (!fetchRequest.ok) {
+      throw new Error(`Request status: ${fetchRequest.status}`);
+    }
+    const fetchedData = await fetchRequest.json();
+    console.log(fetchedData);
+  }
+  catch (error) {
+    console.error('Place details couldn\'t be fetched', error);
+  }
+}
 document.addEventListener('DOMContentLoaded', () => {
-  /* DO SOMETHING */
+  
 });
