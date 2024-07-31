7a94d6ef5d573c30777a548c5724684f916e7183
kriverao98 <riveraocasiokoral29@gmail.com>
2024-07-31 03:40:01 -0400

    modified the app.py file to include CORS as to be able to test my code on safari, implemented the fetchPlaceDetails function to fetch the places api. currently working on populateHost function.

--- 03-frontend/base_files/scripts.js
+++ 03-frontend/base_files/scripts.js
@@ -3,22 +3,43 @@
   Please, follow the project instructions to complete the tasks.
 */
 
-async function fetchPlace_details() {
-  const placeDetailsApi = "/places";
+  async function fetchPlaceDetails() {
+/*
+This function fetches the places API.
+*/
+    const placeDetailsApi = "http://127.0.0.1:5000/places";
 
-  try {
-    const fetchRequest = await fetch (placeDetailsApi);
+    try {
+      const fetchRequest = await fetch (placeDetailsApi);
 
-    if (!fetchRequest.ok) {
-      throw new Error(`Request status: ${fetchRequest.status}`);
+      if (!fetchRequest.ok) {
+        throw new Error(`Request status: ${fetchRequest.status}`);
+      }
+      const fetchedData = await fetchRequest.json();
+      return fetchedData;
     }
-    const fetchedData = await fetchRequest.json();
-    console.log(fetchedData);
-  }
-  catch (error) {
-    console.error('Place details couldn\'t be fetched', error);
-  }
-}
+    catch (error) {
+      console.error('Place details couldn\'t be fetched.', error);
+    }
+  };
+
+  async function populateHost(host_name) {
+    try {
+      const place_details = await fetchPlaceDetails();
+
+      if (!place_details) {
+        throw new Error(`Place details could not be loaded.`);
+      }
+      const firstPlace = placeDetails[0];
+      const detailsContainer = document.querySelector('.place-details');
+      if (detailsContainer) {
+        detailsContainer.innerHTML = 
+      }
+    }
+
+  };
+
 document.addEventListener('DOMContentLoaded', () => {
-  
+  fetchPlace_details();
+  populateHost();
 });
--- 03-frontend/mock-api/app.py
+++ 03-frontend/mock-api/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_cors import CORS
 import json
 from uuid import uuid4
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
 
+CORS(app, resources={r"/*": {"origins": "*"}})
 jwt = JWTManager(app)
 
 with open('data/users.json') as f:
