126c99a29455f280c34be4c90cdbd714cfc245c3
kriverao98 <riveraocasiokoral29@gmail.com>
2024-07-31 21:53:23 -0400

    modified index and script to populate place cards dynamically

--- 03-frontend/base_files/scripts.js
+++ 03-frontend/base_files/scripts.js
@@ -23,23 +23,57 @@ This function fetches the places API.
     }
   };
 
-  async function populateHost(host_name) {
+  async function cardsDisplay() {
+  /*Function to display cards using place-card */
+  try {
+    const place_details = await fetchPlaceDetails();
+
+    if (!place_details) {
+      throw new Error(`Place details could not be loaded.`);
+    }
+    const displayCard = document.getElementsByClassName('place_card');
+    if (!displayCard) {
+      throw new Error(`Display card is empty.`)
+    }
+    else {
+      return displayCard
+    }
+  }
+  catch {
+    console.error(`An error occurred:`, error.message);
+  }
+  }
+
+  async function populatePlaceCard() {
     try {
       const place_details = await fetchPlaceDetails();
 
       if (!place_details) {
         throw new Error(`Place details could not be loaded.`);
       }
-      const firstPlace = placeDetails[0];
-      const detailsContainer = document.querySelector('.place-details');
-      if (detailsContainer) {
-        detailsContainer.innerHTML = 
+      else {
+        const detailsDiv = document.createElement('div');
+        const base_files = "base_files/images"
+        detailsDiv.setAttribute('class', 'place-card');
+
+        detailsDiv.innerHTML = `
+        <img src="${base_files}/images" class="place-image-large"
+        alt="Large Place Image">
+        <div class="place-info">
+          <h3>${place_details.description}</h3>
+          <p>Price per night:${place_details.price_per_night}</p>
+          <p>Location:${place_details.city_name} + ${place_details.country_name}</P>
+        `;
+        document.body.appendChild(detailsDiv);
       }
     }
-
+    catch {
+      console.error('An error occurred', error.message);
+    }
   };
 
 document.addEventListener('DOMContentLoaded', () => {
   fetchPlace_details();
+  cardsDisplay();
   populateHost();
 });
